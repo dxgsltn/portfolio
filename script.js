adbc14af7718ae34463de6ab441276ab835ab077
dxgsltn <163827345+dxgsltn@users.noreply.github.com>
2024-03-26 16:19:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function darkMode() {
+  let element = document.body;
+  element.className = "dark-mode";
+}
+function lightMode() {
+  let element = document.body;
+  element.className = "light-mode";
+}
